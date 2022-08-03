6cfa2213366091b7dd69781db10acf86499cea00
scx567888 <scx567888@outlook.com>
2022-08-03 16:53:40 +0800

    Dev (#28)

---
+++ src/test/java/cool/scx/test/json/JsonParserTest.java
@@ -0,0 +1,49 @@
+package cool.scx.test.json;
+
+import cool.scx.config.ScxEnvironment;
+import cool.scx.learn.json.JsonParser;
+import cool.scx.util.ObjectUtils;
+import cool.scx.util.StopWatch;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class JsonParserTest {
+
+    public static void main(String[] args) throws IOException {
+        test1();
+    }
+
+    @Test
+    private static void test1() throws IOException {
+        var environment = new ScxEnvironment(JsonParserTest.class);
+        var json = Files.readString(environment.getPathByAppRoot("AppRoot:test.json"));
+
+        StopWatch.start("666");
+        Object s = "";
+        for (int i = 0; i < 999; i++) {
+            s = JsonParser.read(json);
+            s = ObjectUtils.jsonMapper().readTree(json);
+        }
+
+        Object o = null;
+
+        StopWatch.start("123");
+        for (int i = 0; i < 99999; i++) {
+            o = JsonParser.read(json);
+        }
+        System.out.println(StopWatch.stopToMillis("123"));
+
+        System.out.println(o);
+
+        StopWatch.start("1");
+        for (int i = 0; i < 99999; i++) {
+            o = ObjectUtils.jsonMapper().readTree(json);
+        }
+        System.out.println(StopWatch.stopToMillis("1"));
+
+        System.out.println(o);
+    }
+
+}
