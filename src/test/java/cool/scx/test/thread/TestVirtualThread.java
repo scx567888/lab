2f5728b8fbeb5789d1a2bbf664bca688a2d943b9
scx567888 <scx567888@outlook.com>
2022-08-29 14:56:30 +0800

    dev

--- src/test/java/cool/scx/test/thread/TestVirtualThread.java
+++ src/test/java/cool/scx/test/thread/TestVirtualThread.java
@@ -3,21 +3,40 @@ package cool.scx.test.thread;
 import cool.scx.util.exception.ScxExceptionHelper;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
 public class TestVirtualThread {
 
     public static void main(String[] args) throws InterruptedException {
         test1();
+        test2();
     }
 
     @Test
     public static void test1() throws InterruptedException {
-        for (int i = 0; i < 999; i++) {
-            int finalI = i;
-            Thread.startVirtualThread(() -> {
+        var l = new InheritableThreadLocal<Integer>();
+        var vv = IntStream.range(0, 99).mapToObj(i -> {
+            l.set(i);
+            return Thread.startVirtualThread(() -> {
                 ScxExceptionHelper.ignore(() -> Thread.sleep(10));
-                System.out.println(finalI + " : " + Thread.currentThread());
-            }).join();
+                System.out.println(i + " : " + l.get());
+            });
+        }).toList();
+        for (var c : vv) {
+            c.join();
         }
+    }
+
+    @Test
+    public static void test2() {
+        var tl = new InheritableThreadLocal<String>();
+        tl.set("不是空");
+        CompletableFuture.runAsync(() -> {
+            System.out.println(Thread.currentThread());
+            // java 19 之前 则会为空
+            System.out.println(tl.get());
+        }).join();
     }
 
 }
