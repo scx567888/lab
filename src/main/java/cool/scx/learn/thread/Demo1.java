813fb7c375c6046f5d172a15dafb5734dfcccf0c
scx567888 <scx567888@outlook.com>
2022-11-27 15:41:22 +0800

    dev

--- src/main/java/cool/scx/learn/thread/Demo1.java
+++ src/main/java/cool/scx/learn/thread/Demo1.java
@@ -5,26 +5,26 @@ import java.util.concurrent.ForkJoinPool;
 public class Demo1 {
 
     public static void main(String[] args) throws InterruptedException {
-//        Runnable r = () -> System.out.println(Thread.currentThread());
-//
-//        var pThread =
-//                Thread.ofPlatform()
-//                        .unstarted(r);
-//        pThread.start();
-//        pThread.join();
-//
-//        var vThread =
-//                Thread.ofVirtual()
-//                        .unstarted(r);
-//        vThread.start();
-//        vThread.join();
-//
-//        System.out.println(vThread.getClass());
-//
-//        var task = ForkJoinPool.commonPool()
-//                .submit(r);
-//
-//        task.join();
+        Runnable r = () -> System.out.println(Thread.currentThread());
+
+        var pThread =
+                Thread.ofPlatform()
+                        .unstarted(r);
+        pThread.start();
+        pThread.join();
+
+        var vThread =
+                Thread.ofVirtual()
+                        .unstarted(r);
+        vThread.start();
+        vThread.join();
+
+        System.out.println(vThread.getClass());
+
+        var task = ForkJoinPool.commonPool()
+                .submit(r);
+
+        task.join();
 
 
     }
