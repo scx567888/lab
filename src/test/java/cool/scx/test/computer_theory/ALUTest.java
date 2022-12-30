e1d156769b206cdb9a77b2e5df65b3a611139f5d
scx567888 <scx567888@outlook.com>
2022-12-30 13:24:46 +0800

    dev

--- src/test/java/cool/scx/test/computer_theory/ALUTest.java
+++ src/test/java/cool/scx/test/computer_theory/ALUTest.java
@@ -1,12 +1,12 @@
 package cool.scx.test.computer_theory;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.Arrays;
 
 import static cool.scx.learn.computer_theory.ALU.*;
 import static cool.scx.learn.computer_theory.Utils.binaryToDecimal;
 import static cool.scx.learn.computer_theory.Utils.decimalToBinary;
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * 模拟 算术逻辑单元
@@ -20,57 +20,45 @@ public class ALUTest {
     @Test
     public static void test1() {
 
-        System.out.println("测试半加法器 (halfAdder) :");
+        //测试半加法器 (halfAdder)
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                printHalfAdder(i, j);
+                assertEquals(i + j, binaryToDecimal(halfAdder(i, j)));
             }
         }
 
-        System.out.println("测试全加法器 (fullAdder) :");
+        // 测试全加法器 (fullAdder)
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
-                    printFullAdder(i, j, k);
+                    assertEquals(i + j + k, binaryToDecimal(fullAdder(i, j, k)));
                 }
             }
         }
 
-        System.out.println("8 位行波进位加法器 (8BitRippleCarryAdder) :");
+        // 8 位行波进位加法器 (8BitRippleCarryAdder)
         for (int i = 0; i < 256; i++) {
             for (int j = 0; j < 256; j++) {
                 var a = decimalToBinary(i);
                 var b = decimalToBinary(j);
                 try {
                     var r = _8BitRippleCarryAdder(a, b);
-                    System.out.println(i + " + " + j + " = " + binaryToDecimal(r));
+                    assertEquals(i + j, binaryToDecimal(r));
                 } catch (Exception e) {
-                    System.out.println(i + " + " + j + " = " + "数据溢出");
+                    // 数据溢出
                 }
             }
         }
 
-        System.out.println("判断是否为 0 (isZero) :");
+        // 判断是否为 0 (isZero)
         for (int i = 0; i < 256; i++) {
             var a = decimalToBinary(i);
             var r = isZero(a);
-            System.out.println(i + " 是否为 0 ? " + (r ? "是" : "否"));
+            if ((i == 0) != r) {
+                Assert.fail("错误 !!!");
+            }
         }
 
-    }
-
-    /**
-     * 打印半加法器的计算过程
-     */
-    public static void printHalfAdder(int a, int b) {
-        System.out.println(a + " + " + b + " -> " + Arrays.toString(halfAdder(a, b)));
-    }
-
-    /**
-     * 打印全加法器的计算过程
-     */
-    public static void printFullAdder(int a, int b, int c) {
-        System.out.println(a + " + " + b + " + " + c + " -> " + Arrays.toString(fullAdder(a, b, c)));
     }
 
 }
