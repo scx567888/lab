1dadd070e2cb26338b7b89bc57ec6fbf3a228486
scx567888 <scx567888@outlook.com>
2024-10-08 12:19:21 +0800

    dev

---
+++ net/src/main/java/cool/scx/net/ScxTCPClientOptions.java
@@ -0,0 +1,20 @@
+package cool.scx.net;
+
+public class ScxTCPClientOptions {
+    
+    private TLS tls;
+
+    public ScxTCPClientOptions() {
+        this.tls = null;
+    }
+
+    public TLS tls() {
+        return tls;
+    }
+
+    public ScxTCPClientOptions tls(TLS tls) {
+        this.tls = tls;
+        return this; 
+    }
+    
+}
