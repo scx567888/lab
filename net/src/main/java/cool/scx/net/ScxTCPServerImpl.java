7496ad5f0a79f33dbf8b9994cc8b38fccdc9d779
scx567888 <scx567888@outlook.com>
2024-10-08 08:53:58 +0800

    dev

---
+++ net/src/main/java/cool/scx/net/ScxTCPServerImpl.java
@@ -0,0 +1,73 @@
+package cool.scx.net;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Consumer;
+
+public class ScxTCPServerImpl implements ScxTCPServer {
+
+    private final Thread serverThread;
+    private Consumer<Socket> connectHandler;
+    private ServerSocket serverSocket;
+    private boolean running;
+
+    public ScxTCPServerImpl() {
+        this.serverThread = Thread.ofPlatform().unstarted(this::listen);
+    }
+
+    @Override
+    public ScxTCPServer onConnect(Consumer<Socket> connectHandler) {
+        this.connectHandler = connectHandler;
+        return this;
+    }
+
+    @Override
+    public void start() {
+        if (running) {
+            throw new IllegalStateException("Server is already running");
+        }
+
+        try {
+            this.serverSocket = new ServerSocket();
+            this.serverSocket.bind(new InetSocketAddress(8080));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        running = true;
+
+        serverThread.start();
+    }
+
+    @Override
+    public void stop() {
+        if (!running) {
+            return;
+        }
+
+        running = false;
+
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        serverThread.interrupt();
+    }
+
+    private void listen() {
+        while (running) {
+            try {
+                var socket = this.serverSocket.accept();
+                Thread.ofVirtual().start(() -> connectHandler.accept(socket));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+
+}
