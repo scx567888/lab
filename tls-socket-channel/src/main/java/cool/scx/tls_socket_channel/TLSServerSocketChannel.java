cae6ffaf2279528ae674705d69531d2df2c4f0e6
scx567888 <scx567888@outlook.com>
2024-11-06 14:56:32 +0800

    dev

--- tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/TLSServerSocketChannel.java
+++ tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/TLSServerSocketChannel.java
@@ -3,14 +3,20 @@ package cool.scx.tls_socket_channel;
 import cool.scx.net.tls.TLS;
 
 import java.io.IOException;
+import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 public class TLSServerSocketChannel extends AbstractServerSocketChannel {
 
     private final TLS tls;
 
-    public TLSServerSocketChannel(TLS tls) throws IOException {
+    public TLSServerSocketChannel(TLS tls, ServerSocketChannel socketChannel) throws IOException {
+        super(socketChannel);
         this.tls = tls;
+    }
+
+    public TLSServerSocketChannel(TLS tls) throws IOException {
+        this(tls, ServerSocketChannel.open());
     }
 
     @Override
