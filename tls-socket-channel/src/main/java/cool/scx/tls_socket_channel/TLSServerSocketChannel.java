021181fe31ecbc209e339fada2a3d18aecc0fe12
scx567888 <scx567888@outlook.com>
2024-11-06 14:22:00 +0800

    dev

---
+++ tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/TLSServerSocketChannel.java
@@ -0,0 +1,73 @@
+package cool.scx.tls_socket_channel;
+
+import cool.scx.net.tls.TLS;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
+
+public class TLSServerSocketChannel extends ServerSocketChannel {
+
+    private final TLS tls;
+    private final ServerSocketChannel serverSocketChannel;
+
+    public TLSServerSocketChannel(TLS tls) throws IOException {
+        super(SelectorProvider.provider());
+        this.tls = tls;
+        this.serverSocketChannel = ServerSocketChannel.open();
+    }
+
+    @Override
+    public ServerSocketChannel bind(SocketAddress local, int backlog) throws IOException {
+        this.serverSocketChannel.bind(local, backlog);
+        return this;
+    }
+
+    @Override
+    public <T> ServerSocketChannel setOption(SocketOption<T> name, T value) throws IOException {
+        this.serverSocketChannel.setOption(name, value);
+        return this;
+    }
+
+    @Override
+    public <T> T getOption(SocketOption<T> name) throws IOException {
+        return this.serverSocketChannel.getOption(name);
+    }
+
+    @Override
+    public Set<SocketOption<?>> supportedOptions() {
+        return this.serverSocketChannel.supportedOptions();
+    }
+
+    @Override
+    public ServerSocket socket() {
+        return this.serverSocketChannel.socket();
+    }
+
+    @Override
+    public SocketChannel accept() throws IOException {
+        var accept = this.serverSocketChannel.accept();
+        return new TLSSocketChannel(tls, accept);
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() throws IOException {
+        return serverSocketChannel.getLocalAddress();
+    }
+
+    @Override
+    protected void implCloseSelectableChannel() throws IOException {
+        serverSocketChannel.close();
+    }
+
+    @Override
+    protected void implConfigureBlocking(boolean block) throws IOException {
+        serverSocketChannel.configureBlocking(block);
+    }
+
+}
