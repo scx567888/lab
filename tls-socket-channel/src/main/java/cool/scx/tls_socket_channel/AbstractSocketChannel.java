8d98120244f2aefac68c74412ae51bfa6480a537
scx567888 <scx567888@outlook.com>
2024-11-06 14:42:14 +0800

    dev

---
+++ tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/AbstractSocketChannel.java
@@ -0,0 +1,99 @@
+package cool.scx.tls_socket_channel;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Set;
+
+public abstract class AbstractSocketChannel extends SocketChannel {
+    
+    protected final SocketChannel socketChannel;
+
+    protected AbstractSocketChannel(SocketChannel socketChannel) {
+        super(SelectorProvider.provider());
+        this.socketChannel = socketChannel;
+    }
+
+    protected AbstractSocketChannel() throws IOException {
+        this(SocketChannel.open());
+    }
+
+    @Override
+    public SocketChannel bind(SocketAddress local) throws IOException {
+        return socketChannel.bind(local);
+    }
+
+    @Override
+    public <T> SocketChannel setOption(SocketOption<T> name, T value) throws IOException {
+        return socketChannel.setOption(name, value);
+    }
+
+    @Override
+    public <T> T getOption(SocketOption<T> name) throws IOException {
+        return socketChannel.getOption(name);
+    }
+
+    @Override
+    public Set<SocketOption<?>> supportedOptions() {
+        return socketChannel.supportedOptions();
+    }
+
+    @Override
+    public SocketChannel shutdownInput() throws IOException {
+        return socketChannel.shutdownInput();
+    }
+
+    @Override
+    public SocketChannel shutdownOutput() throws IOException {
+        return socketChannel.shutdownOutput();
+    }
+
+    @Override
+    public Socket socket() {
+        return socketChannel.socket();
+    }
+
+    @Override
+    public boolean isConnected() {
+        return socketChannel.isConnected();
+    }
+
+    @Override
+    public boolean isConnectionPending() {
+        return socketChannel.isConnectionPending();
+    }
+
+    @Override
+    public boolean connect(SocketAddress remote) throws IOException {
+        return socketChannel.connect(remote);
+    }
+
+    @Override
+    public boolean finishConnect() throws IOException {
+        return socketChannel.finishConnect();
+    }
+
+    @Override
+    public SocketAddress getRemoteAddress() throws IOException {
+        return socketChannel.getRemoteAddress();
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() throws IOException {
+        return socketChannel.getLocalAddress();
+    }
+
+    @Override
+    protected void implCloseSelectableChannel() throws IOException {
+        socketChannel.close();
+    }
+
+    @Override
+    protected void implConfigureBlocking(boolean block) throws IOException {
+        socketChannel.configureBlocking(block);
+    }
+    
+}
--- tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/TLSSocketChannel.java
+++ tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/TLSSocketChannel.java
@@ -3,87 +3,24 @@ package cool.scx.tls_socket_channel;
 import cool.scx.net.tls.TLS;
 
 import java.io.IOException;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.SocketOption;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-import java.nio.channels.spi.SelectorProvider;
-import java.util.Set;
 
-public class TLSSocketChannel extends SocketChannel {
+public class TLSSocketChannel extends AbstractSocketChannel {
 
     private final TLS tls;
-    private final SocketChannel socketChannel;
 
     protected TLSSocketChannel(TLS tls, SocketChannel socketChannel) {
-        super(SelectorProvider.provider());
+        super(socketChannel);
         this.tls = tls;
-        this.socketChannel = socketChannel;
     }
 
     protected TLSSocketChannel(TLS tls) throws IOException {
-        this(tls, SocketChannel.open());
+        this.tls = tls;
     }
 
-    @Override
-    public SocketChannel bind(SocketAddress local) throws IOException {
-        return socketChannel.bind(local);
-    }
+    public void startHandshake() throws IOException {
 
-    @Override
-    public <T> SocketChannel setOption(SocketOption<T> name, T value) throws IOException {
-        return socketChannel.setOption(name, value);
-    }
-
-    @Override
-    public <T> T getOption(SocketOption<T> name) throws IOException {
-        return socketChannel.getOption(name);
-    }
-
-    @Override
-    public Set<SocketOption<?>> supportedOptions() {
-        return socketChannel.supportedOptions();
-    }
-
-    @Override
-    public SocketChannel shutdownInput() throws IOException {
-        return socketChannel.shutdownInput();
-    }
-
-    @Override
-    public SocketChannel shutdownOutput() throws IOException {
-        return socketChannel.shutdownOutput();
-    }
-
-    @Override
-    public Socket socket() {
-        return socketChannel.socket();
-    }
-
-    @Override
-    public boolean isConnected() {
-        return socketChannel.isConnected();
-    }
-
-    @Override
-    public boolean isConnectionPending() {
-        return socketChannel.isConnectionPending();
-    }
-
-    @Override
-    public boolean connect(SocketAddress remote) throws IOException {
-        return socketChannel.connect(remote);
-    }
-
-    @Override
-    public boolean finishConnect() throws IOException {
-        return socketChannel.finishConnect();
-    }
-
-    @Override
-    public SocketAddress getRemoteAddress() throws IOException {
-        return socketChannel.getRemoteAddress();
     }
 
     @Override
@@ -105,24 +42,5 @@ public class TLSSocketChannel extends SocketChannel {
     public long write(ByteBuffer[] srcs, int offset, int length) throws IOException {
         return 0;
     }
-
-    @Override
-    public SocketAddress getLocalAddress() throws IOException {
-        return socketChannel.getLocalAddress();
-    }
-
-    @Override
-    protected void implCloseSelectableChannel() throws IOException {
-        socketChannel.close();
-    }
-
-    @Override
-    protected void implConfigureBlocking(boolean block) throws IOException {
-        socketChannel.configureBlocking(block);
-    }
-
-    public void startHandshake() throws IOException {
-
-    }
-
+    
 }
