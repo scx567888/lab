2fb4d6edabddbe295b7804d7d02a37a8718b6e53
scx567888 <scx567888@outlook.com>
2024-11-06 14:06:11 +0800

    dev

---
+++ tls-socket-channel/src/main/java/cool/scx/tls_socket_channel/InputStreamWrapper.java
@@ -0,0 +1,32 @@
+package cool.scx.tls_socket_channel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+
+public class InputStreamWrapper extends InputStream {
+
+    private final ReadableByteChannel channel;
+
+    public InputStreamWrapper(ReadableByteChannel channel) {
+        this.channel = channel;
+    }
+
+    @Override
+    public int read() throws IOException {
+        var buffer = ByteBuffer.allocate(1);
+        int read = channel.read(buffer);
+        if (read == -1) {
+            return -1;
+        }
+        return buffer.array()[0] & 0xFF;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        var buffer = ByteBuffer.wrap(b, off, len);
+        return channel.read(buffer);
+    }
+
+}
