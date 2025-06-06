0508666b63178915ced6e448c257e2d03cdd6aac
scx567888 <scx567888@outlook.com>
2025-06-06 18:11:14 +0800

    dev

---
+++ neogeo/src/main/java/cool/scx/neogeo/NeoGeoCartridge.java
@@ -0,0 +1,34 @@
+package cool.scx.neogeo;
+
+public class NeoGeoCartridge {
+
+    // 主程序 ROM, 最多2块
+    public byte[] programRom1;
+    public byte[] programRom2;
+
+    // 图像 ROM, 最多8块
+    public byte[] characterRom1;
+    public byte[] characterRom2;
+    public byte[] characterRom3;
+    public byte[] characterRom4;
+    public byte[] characterRom5;
+    public byte[] characterRom6;
+    public byte[] characterRom7;
+    public byte[] characterRom8;
+
+    // 声音 CPU 程序 ROM, 1块
+    public byte[] soundCpuRom;
+
+    // 音频采样 ROM, 最多4块
+    public byte[] soundSampleRom1;
+    public byte[] soundSampleRom2;
+    public byte[] soundSampleRom3;
+    public byte[] soundSampleRom4;
+
+    // 固定布局 ROM, 1块
+    public byte[] fixedLayerRom;
+
+    // 安全芯片加密ROM, 1块
+    public byte[] securityRom;
+
+}
