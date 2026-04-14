8952174a5b910bd289b1957730384b6262fde4f9
scx567888 <scx567888@outlook.com>
2026-04-14 08:44:05 +0800

    代码优化 (#93)

---
+++ cocos-decoder/src/test/java/dev/scx/cocos_decoder/test/CocosRenamerTest.java
@@ -0,0 +1,11 @@
+package dev.scx.cocos_decoder.test;
+
+import java.io.IOException;
+
+import static dev.scx.cocos_decoder.CocosRenamer.traverseRename;
+
+public class CocosRenamerTest {
+    public static void main(String[] args) throws IOException {
+        traverseRename("xxx", "xxxx");
+    }
+}
